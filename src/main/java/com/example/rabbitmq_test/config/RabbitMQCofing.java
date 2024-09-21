d2d9f33fa964e6a1aa4349f1dba6c87d6a8cc801
cgcm070 <cesar.gabriel.martinezs7@gmail.com>
2024-09-21 22:52:53 +0200

    Primeras configuraciones del properties y Clase de config de RabbiMQ

---
+++ src/main/java/com/example/rabbitmq_test/config/RabbitMQCofing.java
@@ -0,0 +1,49 @@
+package com.example.rabbitmq_test.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class RabbitMQCofing {
+    /*
+        Valores de propiedades de application.properties
+        Conection Fctory los configura automaticamente
+        Rabbit Template los configura automaticamente
+        AmqpAdmin los configura automaticamente
+        RabbitAdmin los configura automaticamente
+   */
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key.name}")
+    private String routingKey = "rabbitmq.routing.key";
+
+    // bean para rabbitmq queue
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    // bean para rabbitmq exchange
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    // Binding para queue e exchange usanandp routing key
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+    }
+
+
+}
