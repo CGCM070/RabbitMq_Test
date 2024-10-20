8ec77eea694820e8e8c22917f23c1fe37d66d033
cgcm070 <cesar.gabriel.martinezs7@gmail.com>
2024-10-20 18:59:42 +0200

    Comentado la clase de RabbitMQConsumer.java por que los mensajesse estan duplicando de alguna manera

--- src/main/java/com/example/rabbitmq_test/consumer/RabbitMQConsumer.java
+++ src/main/java/com/example/rabbitmq_test/consumer/RabbitMQConsumer.java
@@ -1,27 +1,27 @@
-package com.example.rabbitmq_test.consumer;
-
-import com.example.rabbitmq_test.config.MyWebSocketHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-
-@Service
-public class RabbitMQConsumer {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
-
-    @Autowired
-    private MyWebSocketHandler webSocketHandler;
-
-    @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void consumeMessage(String message) {
-        LOGGER.info("Message received: {}", message);
-        try {
-            webSocketHandler.broadcastMessage(message);
-        } catch (Exception e) {
-            LOGGER.error("Error broadcasting message: ", e);
-        }
-    }
-}
+//package com.example.rabbitmq_test.consumer;
+//
+//import com.example.rabbitmq_test.config.MyWebSocketHandler;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Service;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//@Service
+//public class RabbitMQConsumer {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
+//
+//    @Autowired
+//    private MyWebSocketHandler webSocketHandler;
+//
+//    @RabbitListener(queues = "${rabbitmq.queue.name}")
+//    public void consumeMessage(String message) {
+//        LOGGER.info("Message received: {}", message);
+//        try {
+//            webSocketHandler.broadcastMessage(message);
+//        } catch (Exception e) {
+//            LOGGER.error("Error broadcasting message: ", e);
+//        }
+//    }
+//}
